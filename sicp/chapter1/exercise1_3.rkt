6946f1e9587a6d9c6dade3a5b732c706545e9236
Mona Lisa <mona@lisa.com>
2018-07-31 20:09:47 +0300

    Refactoring exercise1_3

--- sicp/chapter1/exercise1_3.rkt
+++ sicp/chapter1/exercise1_3.rkt
@@ -1,6 +1,6 @@
 #lang racket
-(define (square a) (* a a))
 (define (sum-of-squares a b c)
-  (cond ((and (> a b) (> b c)) (+ (square a) (square b)))
-        ((and (> a b) (> c b)) (+ (square a) (square c)))
+  (define (square a) (* a a))
+  (cond ((and (> a b) (> c b)) (+ (square a) (square c)))
+        ((and (> a c) (> b c)) (+ (square a) (square b)))
         ((and (> b a) (> c a)) (+ (square b) (square c)))))
